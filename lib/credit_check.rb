e9bfaebf65ea415a33e409ef07f8e8f951dc5061
Billy Wallace <wallace.billy1@gmail.com>
2024-02-01 12:39:10 -0700

    Build iteration 1

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,34 @@
-card_number = "5541808923795240"
+def check_if_valid(card_number)
+    array = transform_to_array(card_number)
+    double_array = double_every_other(array)
+    array_sum = sum_digits_greater_than_9(double_array).sum
+    if array_sum % 10 == 0
+        "The number #{card_number} is valid!"
+    else
+        "The number #{card_number} is invalid!"
+    end
+end
 
-# Your Luhn Algorithm Here
+def transform_to_array(card_number)
+    card_number.to_i.digits.reverse
+end
 
-# Output
-## If it is valid, print "The number [card number] is valid!"
-## If it is invalid, print "The number [card number] is invalid!"
+def double_every_other(array)
+    array.each_index do |index|
+        if index.even? || index.zero?
+            array[index] *= 2
+        end
+    end
+end
+
+def sum_digits_greater_than_9(double_array)
+    double_array.map do |number|
+        if number > 9
+            number.digits.sum
+        else
+            number
+        end
+    end
+end
+
+p check_if_valid("5541808923795240")
